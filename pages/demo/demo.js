40073fd3f393b81a1e15308645055d295a69e9b3
widesky0 <kingyan09@qq.com>
2019-09-02 22:45:38 +0800

    '016-游戏平台新闻发布展示'

---
+++ pages/demo/demo.js
@@ -0,0 +1,81 @@
+// pages/demo/demo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    condition:true,
+    score:40,
+    studentScores:[90,80,70,60],
+    studentArr:[
+      {
+        name:"张三"
+      },
+      {
+        name: "李四"
+      }
+    ],
+
+    subjects:[
+      {id:1,first:"语文"},
+      {id:2,second:"数学"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,155 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gameLists:[
+      {
+        img:'y1.png',
+        title:'纸牌游戏'
+      },
+      {
+        img: 'y1.png',
+        title: '飞机游戏'
+      }
+    ],
+    navigatorArr: ['王者荣耀', '球球大作战', '皇室战争', '梦幻西游', '穿越火线', '我的世界', '火隐忍者', '棋牌世界', '部落冲突', '街头篮球'],
+    img1: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563327679&di=7c2e1250f9d8e2695c722df17f1605c2&imgtype=jpg&er=1&src=http%3A%2F%2Fnewsimg.5054399.com%2Fuploads%2F091028%2F1256702808_4ae7c3586ba69.jpeg',
+    img2: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562732973486&di=7ffc3dc9a96a6fab53d3691f66e132b2&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F17%2F90%2F23%2F55a7b48d86771_1024.jpg',
+    img3: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562732983580&di=2cd9c09b7bfb6e1a1fcb61f99da6ad8d&imgtype=0&src=http%3A%2F%2Fimg3.redocn.com%2Ftupian%2F20140815%2Fhuoyanqichecemiantaobaodianzhaobeijing_2736789.jpg',
+
+    navMenu:[
+      {
+        img:'t1.png',
+        title:'秒杀',
+        url:''
+      },
+      {
+        img: 't2.png',
+        title: '对战',
+        url: ''
+      },
+      {
+        img: 't3.png',
+        title: '收藏',
+        url: ''
+      },
+      {
+        img: 't4.png',
+        title: '登录',
+        url: ''
+      },
+      {
+        img: 't5.png',
+        title: '设置',
+        url: ''
+      },
+    ],
+
+    navigatorScroll:[
+      {
+        img: 'u1.png',
+        title: '秒杀',
+        url: ''
+      },
+      {
+        img: 'u2.png',
+        title: '对战',
+        url: ''
+      },
+      {
+        img: 'u3.png',
+        title: '收藏',
+        url: ''
+      },
+      {
+        img: 'u4.png',
+        title: '日志',
+        url: ''
+      },
+      {
+        img: 'u5.png',
+        title: '设置',
+        url: ''
+      },
+      {
+        img: 'u6.png',
+        title: '日志',
+        url: ''
+      },
+      {
+        img: 'u7.png',
+        title: '设置',
+        url: ''
+      },
+      {
+        img: 'u8.png',
+        title: '登录',
+        url: ''
+      },
+      {
+        img: 'u9.png',
+        title: '设置',
+        url: ''
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
