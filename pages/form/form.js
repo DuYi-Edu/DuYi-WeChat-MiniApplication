4fbc50c1b011fb7a11afb26e741975e4be91ddc9
widesky0 <kingyan09@qq.com>
2019-09-02 22:57:38 +0800

    '021-微信表单控件的提交与控件编写'

---
+++ pages/form/form.js
@@ -0,0 +1,85 @@
+// pages/form/form.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 提交事件
+   */
+  formSubmit(e){
+    console.log(e.detail.value)
+    //获取数据
+
+    //处理数据
+
+    //提交数据
+  },
+
+  /**
+   * 重置事件
+   */
+  formReset(e){
+    console.log(e)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
