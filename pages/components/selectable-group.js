879baf41b7fbad637da46d3e8f7f9417d553be73
widesky0 <kingyan09@qq.com>
2019-09-17 20:26:18 +0800

    '097-抽象节点'

---
+++ pages/components/selectable-group.js
@@ -0,0 +1,24 @@
+// pages/components/selectable-group.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    labels:[1,2,3],
+    selected:[false,true,false]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
