8af584099290df92ea581705c184dcca9e10e2ed
widesky0 <kingyan09@qq.com>
2019-09-17 20:15:00 +0800

    '091-组件Component构造器'

---
+++ components/welcome/welcome.js
@@ -0,0 +1,27 @@
+// components/welcome/welcome.js
+Component({
+  externalClasses:['my-class'],
+  options:{
+    multipleSlots:true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
